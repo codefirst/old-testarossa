34df6dba7cb32ee5306f7aad21a70ab2fcd6ab29
suer <suetsugu.r@gmail.com>
2013-01-03 00:46:56 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testarossa::Application.config.secret_token = '44b9c7bf153a33b175964ca31f05a61ed4ec7c1d83ecdfdff28f8155048302a330b11cc6473186939671d56be7799bd3f99445926b9fc013a73a9f529277b34a'
