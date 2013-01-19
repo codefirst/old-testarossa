2a9510f524e4557f703e50374fe3d2b707c79325
mzp <mzpppp@gmail.com>
2013-01-19 22:18:48 +0900

    Apply bootstrap to plan view refs #10

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,13 @@
 module ApplicationHelper
   def breadcrumb(*titles)
-    titles.map(&method(:h)).join("&raquo;")
+    titles.map(&method(:h)).join(" \u00bb ")
+  end
+
+  def nav_to(title, path, params={})
+    if current_page? path then
+      %(<li class="active">#{link_to title, path, params}</li>).html_safe
+    else
+      %(<li>#{link_to title, path, params}</li>).html_safe
+    end
   end
 end
